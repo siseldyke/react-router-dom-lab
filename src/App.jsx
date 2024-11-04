86cc3380a8dd10a17052bcbf70d60fbb9d3c6145
siseldyke <siseldyke@gmail.com>
2024-11-04 15:08:01 -0500

    basic homepage working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
 // src/App.jsx
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import MailboxList from './components/MailboxList';
+import NavBar from './components/NavBar';
+
+const mailboxExample = [
+ { _id: 1, boxSize: 'Small', boxholder: 'Alex'},
+ { _id: 2, boxSize: 'Large', boxholder: 'Bob'},
+ { _id: 3, boxSize: 'Medium', boxholder: 'Jackie'},
+ { _id: 4, boxSize: 'Small', boxholder: 'Frank'},
+ { _id: 5, boxSize: 'Large', boxholder: 'Dustin'}
+
+
+
+
+]
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [mailboxes, setMailboxes] = useState(mailboxExample)
+  
+  
+  
+  
+  return (
+    <>
+    <NavBar />
+    <h1>Mail here</h1>;
+    <Routes>
+    <Route path="/" element={<h2>Home Page</h2>} />
+    <Route path="/mailboxes" element={<MailboxList mailboxes={mailboxes} />} />
+
+
+
+
+
+    </Routes>
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
+  )
 };
 
 export default App;
